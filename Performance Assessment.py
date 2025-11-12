d60dfbe63cdaee3fdef2b659792b070b29b451df
jenhen1082 <jenhen1082@students.ecpi.edu>
2025-11-12 08:55:46 -0500

    Data Types and Basic Math Operators

---
+++ Performance Assessment.py
@@ -0,0 +1,18 @@
+name = input("Please enter your name: ")
+iD = input("Please enter your Student ID: ")
+number1 = int(input("Please enter a whole number: "))
+myNumber = input("Please enter a different second whole number: ")
+number2 = int(myNumber)
+answer = number1 * number2
+print(f'The result of {number1} times {number2} is: {answer:.2f}')
+answer2 = number1 / number2
+print(f'The result of {number1} divided by {number2} is: {answer2:.2f}')
+answer3 = number1 + number2
+print(f'The result of {number1} plus {number2} is: {answer3:.2f}')
+if number1 > number2:
+    print(f'Number 1 is larger than Number 2')
+elif number2 < number2:
+    print(f'Number 2 is larger than Number 2')
+
+print(name)
+print(iD)
